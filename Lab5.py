3717eabee794a38dc1a14868ce966346c2b91796
Chirakit <65070034@kmitl.ac.th>
2023-03-24 10:27:11 +0700

    Archive

--- Lab5.py
+++ Lab5.py
@@ -31,29 +31,67 @@ class BST:
                     prev = start
                     start = start.right
             #เชื่อมโหนด pNew
-            if (prev.right == start):
+            if prev.right == start:
                 prev.right = pNew
             else:
                 prev.left = pNew
 
     def delete(self, data) :
         """delete data from BST"""
-        return
+        start = self.root
+        prev = None
+        if self.is_empty():
+            print("This is an empty tree!!")
+            return False
+        else:
+            while data != start.data and data != None:
+                if data < start.data:
+                    prev = start
+                    start = start.left
+                else:
+                    prev = start
+                    start = start.right
+            if start.left == None and start.right == None:
+                if prev.data > data:
+                    prev.left = None
+                else:
+                    prev.right = None
+            elif start.left != None and start.right != None:
+                checkp = start.left
+                prev = start
+                while checkp.right != None:
+                    prev = checkp
+                    checkp = checkp.right
+                start.data = checkp.data
+                prev.right = None
+            elif start.left != None:
+                checkp = start
+                prev = start
+                while checkp.left != None:
+                    prev = checkp
+                    checkp = checkp.left
+                start.data = checkp.data
+                prev.left = None
+            else:
+                if prev.data > data:
+                    prev.left = start.right
+                else:
+                    prev.right = start.right
 
     def preorder(self, root) :
-        if (root != None):
+        if root != None:
             print("->", root.data, end=" ")
             self.preorder(root.left)
             self.preorder(root.right)
 
     def inorder(self, root) :
-        if (root != None):
+        if root != None:
             self.inorder(root.left)
             print("->", root.data, end=" ")
             self.inorder(root.right)
 
     def postorder(self, root) :
-        if (root != None):
+        if root != None:
             self.postorder(root.left)
             self.postorder(root.right)
             print("->", root.data, end=" ")
@@ -96,6 +134,20 @@ myBST.insert(23)
 myBST.insert(7)
 myBST.insert(10)
 myBST.insert(33)
+myBST.insert(5)
+myBST.insert(13)
+myBST.insert(20)
 myBST.traverse()
-print("Min:", myBST.findMin())
-print("Max:", myBST.findMax())
+print("-------")
+myBST.delete(5)
+myBST.traverse()
+myBST.delete(33)
+myBST.traverse()
+myBST.delete(14)
+myBST.traverse()
+myBST.delete(7)
+myBST.traverse()
+myBST.delete(23)
+myBST.traverse()
+myBST.delete(13)
+myBST.traverse()
